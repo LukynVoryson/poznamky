daaa664ecf069107b1e6f0a30178c929936675f0
LukynVoryson <lukas.voracek@infis.cz>
2025-03-18 08:38:33 +0100

    Add files via upload

---
+++ Příkazy vysvětlování.py
@@ -0,0 +1,45 @@
+#načtení od uživatele
+#input() získá vstup jako řetězec.Čísla je nutné přetypovat.Použijem pokud potřebujeme načíst hodnotu od uživatele.
+
+i=input()
+
+#přetypování
+#Používá se int().Pomáhá převést vstup na správný datový typ.
+
+i=input()            #proměnná je typu string,potřebujeme interger.
+i=int (input())      #Teď je typu interger.
+
+#Typů je více.
+
+#podmínky (if,elif,else)
+#Podmínky potřebujeme, když chceme aby program reagoval na různé situace.
+
+if i == 2:                                               #podmínka se skládá z podmínky if a else
+    print("Nazdar")
+elif i == 2:                                             #pokud potřebujem zadat více než jednu podmínku použijeme elif.
+    print("Ne")
+else:
+    print ("Hello")
+
+#tabulátory jsou také důležité pod podmínkami (můžeme zadat podmínky do podmínek).
+
+#logické operátory (and,or,not)
+#Nutné když chceme kombinovat více podmínek. Používá se při složitějším rozhodování programu.Používají se mezi hodnotami.
+
+if i == 7 or i == 14:
+
+#vypsat
+#Pokud chceme něco vypsat od uživatele, použije se print.
+
+print("hello")
+
+#github příkazy
+#github příkazy spravují kód.Umožňují ukládání a sdílení.
+
+#git push
+#git comit
+#git
+
+#vše okomentovat + podpis odevzat na github
+
+#Lukáš Voráček
---
+++ rok = 2025.py
@@ -0,0 +1,18 @@
+rok = 2025
+text= "veselé vánoceů"
+
+print (f"Vítejte, letos je rok {rok}")
+
+print (f"{text} {rok}.")
+cislo= 453123
+
+#oddělovač
+print (f"{cislo:,}")
+
+#desetinná čísla
+print (f"{cislo:.5}")
+
+prevod=155
+
+#převody
+print(f"{cislo:}")
